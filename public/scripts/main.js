7b08224a038d4e0fc315505e8e2812b4818b9b5d
Admin <Admin@DESKTOP-LC407O9>
2025-10-20 12:58:20 +0200

    poprawki

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,5 @@
 const header = document.querySelector('h1');
 console.log(header);
-header.addEventListener('click', () =>{
+header.addEventListener('click', () => {
     header.style.color = 'green';
-})
+});
