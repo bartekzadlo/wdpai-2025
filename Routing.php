617c0fbd878e15981ddb48de54d80eea835e6e7b
Admin <Admin@DESKTOP-LC407O9>
2025-10-27 13:28:42 +0100

    routing

--- Routing.php
+++ Routing.php
@@ -1,17 +1,43 @@
 <?php
 
-class Routing {
-    public static function run($path) {
-        switch($path) {
-            case 'dashboard':
-                include 'public/views/dashboard.html';
-                break;
-            case 'login':
-                include 'public/views/login.html';
-                break;
-            default:
-                include 'public/views/404.html';
-                break;
-        }
+require_once 'src/controllers/SecurityController.php';
+class Routing
+{
+
+  public static $routes = [
+    'login' => [
+      'controller' => 'SecurityController',
+      'action' => 'login'
+    ],
+    'register' => [
+      'controller' => 'SecurityController',
+      'action' => 'register'
+    ]
+  ];
+
+  public static function run(string $path)
+  {
+
+    switch ($path) {
+      case 'dashboard':
+        include 'public/views/dashboard.html';
+        break;
+      case 'login':
+
+        $controller = Routing::$routes[$path]['controller'];
+        $action = Routing::$routes[$path]['action'];
+
+        $controllerObj = new $controller;
+        $controllerObj->$action();
+
+      case 'register':
+        $controller = Routing::$routes[$path]['controller'];
+        $controllerObj = new $controller;
+        $controllerObj->register();
+        break;
+      default:
+        include 'public/views/404.html';
+        break;
     }
+  }
 }
