f5b30b48e1b636b37be5592726ded930f0189cda
Admin <Admin@DESKTOP-LC407O9>
2025-11-17 13:10:56 +0100

    main.css i wyswietanie danych

--- Routing.php
+++ Routing.php
@@ -23,17 +23,13 @@ class Routing
         include 'public/views/dashboard.html';
         break;
       case 'login':
+      case 'register':
 
         $controller = Routing::$routes[$path]['controller'];
         $action = Routing::$routes[$path]['action'];
 
         $controllerObj = new $controller;
         $controllerObj->$action();
-
-      case 'register':
-        $controller = Routing::$routes[$path]['controller'];
-        $controllerObj = new $controller;
-        $controllerObj->register();
         break;
       default:
         include 'public/views/404.html';
