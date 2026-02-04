f93d61fc640fab8b71c5f42f4997ca8a45dd1e47
Admin <Admin@DESKTOP-LC407O9>
2026-02-04 08:48:59 +0100

    poprawione filtrowanie i wyszukiwanie wydarzen

--- public/scripts/events-data.php
+++ public/scripts/events-data.php
@@ -1,4 +1,4 @@
 <?php
 $events = json_decode(file_get_contents('../../storage/events.json'), true);
-echo 'const eventsData = ' . json_encode($events) . ';';
+echo 'const eventsData = ' . json_encode(array_values($events)) . ';';
 ?>
